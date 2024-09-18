172e4a1cd29f21d090af55210aa379fca75d548b
ali-ledezma <a.22308051281229@cbtis128.edu.mx>
2024-09-18 07:41:39 -0600

    Add files via upload
    codigo perro ejemplo

---
+++ claseperro.py
@@ -0,0 +1,17 @@
+print("Programacion POO")
+
+class Perro:
+
+    def morder(self):
+        print("el perro me mordio")
+    def Datos_perro(self,nombre, edad):
+        print(f" Nombre : {nombre} edad: {edad}")
+            
+            
+pitbull=Perro()
+            
+            
+            
+            
+pitbull.Datos_perro("bobby", 4)
+pitbull.morder()
